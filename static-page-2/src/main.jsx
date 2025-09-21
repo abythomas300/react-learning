23c6b98760667b168813a19f4cd97453ff3e73ac
abythomas300 <abythomas300@gmail.com>
2025-09-21 22:47:13 +0530

    static page 2: declarative vs imperative coding

--- static-page-2/src/main.jsx
+++ static-page-2/src/main.jsx
@@ -4,12 +4,20 @@ import Header from './header'
 
 const root = createRoot(document.querySelector("#rootDiv"))
 
-root.render(
-    <div>
-        <Header />
-        <Navbar />
-    </div>
-)
-// render everything into root Div
 
+root.render(
+        <Header className='header' />
+)
+
+
+// Same above code when written in imperative coding (using js only):
+
+// const header = document.createElement("h2")
+// header.textContent = "Maverick Here"
+// header.className = "header"
+// document.querySelector("#rootDiv").appendChild(header)
+// Declarative is more cleaner and convinient than imperative
+
+// Declarative Coding: We tell the system what to do , it will find a way to make it happen (eg: react)
+// Imperative Coding: We tell every single step for doing something
 
